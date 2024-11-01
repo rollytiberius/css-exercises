99ac60e8428d59ec8f3f8143cc73aa199cced505
rollytiberius <rollytiberius@gmail.com>
2024-11-01 21:03:33 +0000

    added buttons functionality

---
+++ foundations/flex/06-flex-layout/script.js
@@ -0,0 +1,10 @@
+const button = document.getElementsByTagName("button")
+
+button[0].addEventListener("click", () => {
+  alert("Do the Thing!")
+})
+
+
+button[1].addEventListener("click", () => {
+  alert("Do the other thing!")
+})
